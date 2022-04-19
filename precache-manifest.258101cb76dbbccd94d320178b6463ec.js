904470314fbd6538a3366a6b9ad76375326e29f6
ednese <evancours94@gmail.com>
2022-04-19 21:50:07 +0200

    Updates

---
+++ precache-manifest.258101cb76dbbccd94d320178b6463ec.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "739287f0b7403b533bdec5ca325a49d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37f913d3914b9a007620",
+    "url": "/static/css/main.38c60c69.chunk.css"
+  },
+  {
+    "revision": "4290a9c2c5223cd9d296",
+    "url": "/static/js/2.e291ab94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e291ab94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37f913d3914b9a007620",
+    "url": "/static/js/main.fbf02056.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "43ff36cb8e18ba7d7ffb487aed3e28c4",
+    "url": "/static/media/AdobeXd.43ff36cb.png"
+  },
+  {
+    "revision": "14c89a41e5154b4a2920df0115337ad5",
+    "url": "/static/media/React.14c89a41.png"
+  },
+  {
+    "revision": "9fd2334eff070c134459c9ee50375a9d",
+    "url": "/static/media/Screenshot_tesla.9fd2334e.png"
+  },
+  {
+    "revision": "7d3442e95f81368bd1d64316aeae0deb",
+    "url": "/static/media/discord.7d3442e9.png"
+  },
+  {
+    "revision": "7ad576366ae8efbf78a685917d310b9d",
+    "url": "/static/media/easybank.7ad57636.jpg"
+  },
+  {
+    "revision": "c658753158b24acf7df586f40f3fb6ad",
+    "url": "/static/media/kickapp.c6587531.png"
+  },
+  {
+    "revision": "8f7ef6d551fbc6eee4b92fdce17fd362",
+    "url": "/static/media/projectoi.8f7ef6d5.png"
+  },
+  {
+    "revision": "b505b2e90ecc54d6ba0017a0402e16ce",
+    "url": "/static/media/thomas.b505b2e9.jpg"
+  }
+]);
